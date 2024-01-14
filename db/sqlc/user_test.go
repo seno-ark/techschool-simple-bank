ef68b245a36f360622feb1c81e28fe6450a445db
seno-ark <amin.kamsena@gmail.com>
2024-01-15 00:31:53 +0700

    add genereate & check password

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -28,9 +28,12 @@ func TestGetUser(t *testing.T) {
 }
 
 func createRandomUser(t *testing.T) User {
+	hashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username:       util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashedPassword,
 		FullName:       util.RandomOwner(),
 		Email:          util.RandomEmail(),
 	}
