93406aaa2e85b2891db27174efefa6f515ac93ca
seno-ark <amin.kamsena@gmail.com>
2024-01-15 12:35:16 +0700

    add user login endpoint

--- api/main_test.go
+++ api/main_test.go
@@ -2,12 +2,28 @@ package api
 
 import (
 	"os"
+	db "simple-bank/db/sqlc"
+	"simple-bank/util"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
+}
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
 }
